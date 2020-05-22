49288f4edfb83e64db67dbd319942b6260048aee
BenKung <moon791017@gmail.com>
2020-05-22 14:22:01 +0800

    add pages

---
+++ App1/MyPag2.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MyPag2 : ContentPage
+    {
+        public MyPag2()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ App1/MyPage1.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MyPage1 : ContentPage
+    {
+        public MyPage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
